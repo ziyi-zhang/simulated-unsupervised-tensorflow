28beace8599c52f195fa574e36d213c682345b7f
Terence Zhang <terencezhangziyi@gmail.com>
2018-09-10 20:51:48 +0800

    update

--- data/hand_data.py
+++ data/hand_data.py
@@ -1,4 +1,3 @@
-
 class DataLoader(object):
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
--- main.py
+++ main.py
@@ -1,29 +1,32 @@
 import sys
+
 import numpy as np
 import tensorflow as tf
 
-from trainer import Trainer
 from config import get_config
+from trainer import Trainer
 from utils import prepare_dirs, save_config
 
 config = None
 
+
 def main(_):
-  prepare_dirs(config)
+    prepare_dirs(config)
 
-  rng = np.random.RandomState(config.random_seed)
-  tf.set_random_seed(config.random_seed)
+    rng = np.random.RandomState(config.random_seed)
+    tf.set_random_seed(config.random_seed)
 
-  trainer = Trainer(config, rng)
-  save_config(config.model_dir, config)
+    trainer = Trainer(config, rng)
+    save_config(config.model_dir, config)
 
-  if config.is_train:
-    trainer.train()
-  else:
-    if not config.load_path:
-      raise Exception("[!] You should specify `load_path` to load a pretrained model")
-    trainer.test()
+    if config.is_train:
+        trainer.train()
+    else:
+        if not config.load_path:
+            raise Exception("[!] You should specify `load_path` to load a pretrained model")
+        trainer.test()
+
 
 if __name__ == "__main__":
-  config, unparsed = get_config()
-  tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
+    config, unparsed = get_config()
+    tf.app.run(main=main, argv=[sys.argv[0]] + unparsed)
